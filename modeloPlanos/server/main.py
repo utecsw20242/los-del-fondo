113e772056f78fdad986bcdd9580cfae5801c163
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-11-09 13:21:58 -0500

    modeloPlanos/server/main.py: Change to post

--- modeloPlanos/server/main.py
+++ modeloPlanos/server/main.py
@@ -6,10 +6,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/file", response_model=schemas.FileData)
+@app.post("/file", response_model=schemas.FileData)
 def analyze_file(files: schemas.FileBase):
-    coded_image = files.coded_file
-    matches, annotated_file = apply_model(coded_image)
-    body = matches
-    body["annotated_file"] = annotated_file
-    return body
+    try:
+        coded_image = files.coded_file
+        matches, annotated_file = apply_model(coded_image)
+        body = matches
+        body["annotated_file"] = annotated_file
+        return body
+    except Exception as e:
+        return {"message": "failure", "error": str(e)}
