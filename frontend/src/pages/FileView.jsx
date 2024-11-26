fb7c8ba72101227a519a1ae2cd2f4f30994bda5c
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-11-26 14:45:17 -0500

    frontend/src/pages/FileView.jsx: Add basic score

--- frontend/src/pages/FileView.jsx
+++ frontend/src/pages/FileView.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import '../styles/pages/file-view.scss';
 
 const FileView = ({file}) => {
+  let score =0;
+  if (file){
+    const maxVal = Math.max(file.doorNumber, file.windowNumber, file.textNumber);
+    score = ((file.doorNumber * 5 + file.windowNumber * 4 + file.textNumber)/(5*maxVal + 4*maxVal + maxVal))*100;
+  }
   return (
     <div className="file-view">
       {file ? (
@@ -10,6 +15,7 @@ const FileView = ({file}) => {
         <div className="image">
           <p>File Name: {file.surname}</p>
           <p>This image has {file.doorNumber} doors, {file.windowNumber} windows and {file.textNumber} texts.</p>
+          <p> The scores are 5,4 and 1 respectively. So the current score is: {score} %</p>
         </div>
         <div className="action-buttons">
         <div className="info">
